d3940c11688c877352708693be0e5bd6f488831b
Finn Lancaster <finn.lancaster@finnsoftware.net>
2022-10-13 23:02:33 -0400

    fix blog loading bug

--- assets/js/blogloader.js
+++ assets/js/blogloader.js
@@ -5,7 +5,7 @@ var iframeHeight = winHeight-parseInt(headerHeight);
 document.querySelector('#post').height = iframeHeight;
 
 if (location.href.includes('?name=')) {
-    var postPath = "/blog/"+location.href.split('?name=')[1]+".html";
+    var postPath = "https://arc-animal-rescue.github.io/arcanimalrescue.com/blog/"+location.href.split('?name=')[1]+".html";
 
     document.querySelector('#post').src=postPath;
 } else {
