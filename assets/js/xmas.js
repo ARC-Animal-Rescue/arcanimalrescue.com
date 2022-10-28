d3476d398dffc35733dbe8fe78b7799c350f7415
Finn Lancaster <finn.lancaster@finnsoftware.net>
2022-10-28 11:56:02 -0400

    Push new featured pets, controls on xmas pages

--- assets/js/xmas.js
+++ assets/js/xmas.js
@@ -12,3 +12,36 @@ async function uploaded_images(year) {
   
   return images;
 }
+
+async function yearExists(year) {
+  const response = await fetch(`https://api.github.com/repos/ARC-Animal-Rescue/arcanimalrescue.com/contents/static/xmas${year}.html`);
+  return (response.status === 200) ? true : false;
+}
+
+async function nextYear(thisYear) {
+  const nextYear = thisYear + 1;
+  window.open(`https://arcanimalrescue.com/static/xmas${nextYear}.html`, '_self');
+}
+
+async function prevYear(thisYear) {
+  const prevYear = thisYear - 1;
+  window.open(`https://arcanimalrescue.com/static/xmas${prevYear}.html`, '_self');
+}
+
+async function checkControls() {
+  const year = parseInt(window.location.href.split('xmas')[1].split('.')[0]);
+  const next = await yearExists(year + 1);
+  const prev = await yearExists(year - 1);
+  if (!next) {
+    document.getElementById('next').style.color = 'gray';
+    document.getElementById('next').style.cursor = 'default';
+  }
+  if (!prev) {
+    document.getElementById('prev').style.color = 'gray';
+    document.getElementById('prev').style.cursor = 'default';
+  }
+}
+
+window.onload = async function() {
+  await checkControls();
+}
